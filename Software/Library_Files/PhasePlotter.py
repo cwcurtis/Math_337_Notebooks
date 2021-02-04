977b430f709ed1a97478acb7e3ed6c54ee671008
cwcurtis <ccurtis@mail.sdsu.edu>
2021-02-04 10:44:45 -0800

    Group Work

--- Software/Library_Files/PhasePlotter.py
+++ Software/Library_Files/PhasePlotter.py
@@ -14,10 +14,11 @@ class PhasePlotter(object):
         self.fhandle = FileMaker('phase_figs_' + description)
 
     def PhaseField1D(self):
-        dx = 1e-4
+        dx = 1e-3
         nvals = np.int((self.b-self.a)/dx)
         xvals = np.linspace(self.a, self.b, nvals + 1)
         fvals = self.fun(xvals)
+
         ipos = fvals >= 0.
         ineg = fvals < 0.
         xpos = xvals[ipos]
@@ -26,6 +27,7 @@ class PhasePlotter(object):
         nneg = np.size(xneg)
         pjmps = xpos[1:] - xpos[:npos-1]
         pizrs = pjmps > 2.*dx
+
         if np.mod(np.sum(pizrs), 2) > 0:
             zrsl = xpos[:npos-1][pizrs]
             zrsr = xpos[1:][pizrs]
@@ -50,12 +52,13 @@ class PhasePlotter(object):
         plt.plot(xpos, fvals[ipos], color='b')
         plt.plot(xneg, fvals[ineg], color='r')
         plt.plot(xvals, np.zeros(np.size(xvals)), color='k')
-        head_size = .1
+        head_size = .1*(np.max(fvals)-np.min(fvals))
+        dx_arrow = .025*(self.b-self.a)
         for jj in range(nzrs-1):
             if fmds[jj] > 0.:
-                plt.arrow(mds[jj], 0., .1, 0., shape='full', length_includes_head=True, head_width=head_size)
+                plt.arrow(mds[jj], 0., dx_arrow, 0., shape='full', length_includes_head=True, head_width=head_size)
             else:
-                plt.arrow(mds[jj], 0., -.1, 0., shape='full', length_includes_head=True, head_width=head_size)
+                plt.arrow(mds[jj], 0., -1.*dx_arrow, 0., shape='full', length_includes_head=True, head_width=head_size)
 
         for jj in range(1, nzrs-1):
             if fmds[jj-1] > 0. and fmds[jj] < 0.:
